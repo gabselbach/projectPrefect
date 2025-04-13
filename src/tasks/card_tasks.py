bd5968fcb82a60aaefa1ea64f48d97214f5ad9a8
Gabriella Selbach <gabriellaselbach@gmail.com>
2025-04-13 15:46:00 -0300

    fix: change logger

--- src/tasks/card_tasks.py
+++ src/tasks/card_tasks.py
@@ -1,25 +1,29 @@
-from prefect import task, context
 import json
-import time
 import os
+import time
+
+from prefect.logging import get_run_logger
 from src.api.card_client import CardClient
+from prefect import task, context
 
 client = CardClient()
 
 @task(log_prints=True)
 def search_cards():
+    logger = get_run_logger()
     cards = client.get_all_cards()
-    print("Pesquisado todos os dados")
-    print(json.dumps(cards[0], indent=2, ensure_ascii=False))
+    logger.info("Pesquisado todos os dados")
+    logger.info(json.dumps(cards[0], indent=2, ensure_ascii=False))
     return cards
 
 @task(log_prints=True)
 def search_specific_card(data):
+    logger = get_run_logger()
     specific_card = data[0]
     card = client.get_card_by_id(specific_card['id'])
     
-    print("Card específico:")
-    print(f'Name: {card["name"]}')
+    logger.info("Card específico:")
+    logger.info(f'Name: {card["name"]}')
     return card
 
 @task(
@@ -29,16 +33,17 @@ def search_specific_card(data):
     retry_delay_seconds=2
 )
 def search_special_rarity():
+    logger = get_run_logger()
     run_context = context.get_run_context()
     retry_count = run_context.task_run.run_count
     possible_retries = int(os.environ.get("QT_RETRY"))
     
-    print(f"Tentativa número: {retry_count}")
+    logger.info(f"Tentativa número: {retry_count}")
     
     cards = client.get_cards_by_rarity('Special')
     
     if retry_count < possible_retries:
         time.sleep(5)
     
-    print("Busca especial finalizada")
+    logger.info("Busca especial finalizada")
     return cards
